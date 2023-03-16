23e05aa08bb4cc5cbe71d69b2864bc74921e36c7
Tretyakov Ivan <timmitarh@mail.ru>
2023-03-16 22:12:39 +0300

    Completed task #47

---
+++ Task_47/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+/////////////////////////////////////////////////////////////////////////////////
+// Код основной программы
+Console.Clear();
+int rows = GetNumberFromUser("Укажите количество строк массива: ", "Ошибка ввода!");
+int columns = GetNumberFromUser("Укажите количество столбцов массива: ", "Ошибка ввода!");
+double[,] array = GetArray(rows, columns);
+PrintArray(array);
+//////////////////////////////////////////////////////////////////////////////////
+// Описание методов
+
+// Запрашивает у пользователя целое положительное число
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber > 0)
+        {
+            return userNumber;
+        }
+        Console.WriteLine(errorMessage);
+    }
+}
+// создаём двуменый массив, заполненный случайными вещественными числами
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble() * 100, 1);
+        }
+    }
+    return result;
+}
+// выводим в консоль элементы массива построчно
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
